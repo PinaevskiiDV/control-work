dade63eba4c506cae85885e632780e3d6157e5e4
Dmitrii <pinochet37@mail.ru>
2022-10-14 12:11:04 +0300

    wrote down the algorithm

--- Program_File/Program.cs
+++ Program_File/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"hello", "2", "world", ":-)"};
+
+string[] newArray = new string[array.Length];
+CreateNewArray(array);
+PrintNewArray(newArray);
+Console.WriteLine();
+void CreateNewArray(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+}
+void PrintNewArray(string[] array)
+{
+    int length = newArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"{newArray[index]}, ");
+        index++;
+    }
+}
